2fe41a44c1fe3869b0db05b8e522bd3ae3b22cc0
dan2-ux <danhlanhat9@gmail.com>
2025-03-12 12:00:13 +0700

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './Home/Home'
 import List from './List/List'
 import Hotel from './Hotel/Hotel'
